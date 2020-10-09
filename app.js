cdb979fb02c38b41cb1af444ab0ed7b7cbb46c8f
ergouwang <330035385@qq.com>
2020-10-09 12:11:03 +0800

    使用  koa-bodyparser 获取 body 中相关参数

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const parser = require('koa-bodyparser')
 const InitManager = require('./core/init')
+const { params } = require('./app/api/v1/age')
 
 const app = new Koa()
+
+app.use(parser())
 
 process.cwd()
 
--- app/api/v1/name.js
+++ app/api/v1/name.js
@@ -1,7 +1,14 @@
 const Router = require('koa-router')
 const router = new Router()
 
-router.get('/v1/name',(ctx,next)=>{
+router.post('/v1/:id/name',(ctx,next)=>{
+    //获取 url 里面的参数
+    const path = ctx.params
+    //获取问号后面的查询参数
+    const query = ctx.request.query
+    const header = ctx.request.header
+    const body = ctx.request.body
+    
     ctx.body = 'wangzhen hahah'
 })
 
