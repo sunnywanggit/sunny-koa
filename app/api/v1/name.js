8064faafe8689230ef76b31368ac04fe15741512
ergouwang <330035385@qq.com>
2020-10-10 11:17:54 +0800

    定义异常返回格式并进行捕捉

--- app/api/v1/name.js
+++ app/api/v1/name.js
@@ -8,10 +8,17 @@ router.post('/v1/:id/name',(ctx,next)=>{
     const query = ctx.request.query
     const header = ctx.request.header
     const body = ctx.request.body
-    ctx.body = 'wangzhen hahah'
+    if(!query){
+        //注意：error.message 并不是通过点的形式传递进去的，而是通过 Error 这个构造函数传递进去的，这是因为 message 是 Node Error 对象的一个参数
+        const error = new Error('错误信息')
+        //为了符合 JS 的规范，我们这里使用驼峰的形式
+        error.errorCode = 10001
+        error.status = 400
+        error.requestUrl = `${ctx.method} ${ctx.path}`
+        throw error
+    }
 
-    //我们在这里抛出一个异常，然后再全局进行监听并抛出错误提示信息
-    throw new Error('API Exception')
+    ctx.body = 'wangzhen hahah'
 })
 
 module.exports = router
--- middlewares/expection.js
+++ middlewares/expection.js
@@ -2,7 +2,20 @@ const catchError = async (ctx,next)=>{
     try{
         await next()
     }catch(error){
-        ctx.body = '服务器有点问题，请稍等...'
+        /*
+        首先我们需要知道，我们待处理的异常是已知异常还是未知异常
+        已知异常的典型特征是它的 error 具有 error_code 这个属性
+        */
+        if(error.errorCode){
+            ctx.body = {
+                msg:error.message,
+                //这里为了保持接口的兼容，所以我们这里使用下划线
+                error_code:error.errorCode,
+                request:error.requestUrl,
+            }
+            ctx.status = error.status
+
+        }
     }
 }
 
