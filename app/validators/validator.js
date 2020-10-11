3b74410cf3658f5cf15e2d989fe92b98850633d7
ergouwang <330035385@qq.com>
2020-10-11 11:12:17 +0800

    RegisterValidator 完成对 register 接口参数校验

--- app/validators/validator.js
+++ app/validators/validator.js
@@ -11,6 +11,34 @@ class PositiveIntegerValidator extends LinValidator{
     }
 }
 
+class RegisterValidator extends LinValidator{
+    constructor(){
+        super()
+        this.email = [
+            new Rule('isEmail','请填写正确的 Email',)
+        ]
+        this.password1 = [
+            new Rule('isLength','最少6个字符，最多32个字符',{min:6,max:32}),
+            //我们不希望用户的密码里面存在危险的字符，所以我们要指定用户密码的字符范围
+            new Rule('matchs','密码长度必须在6~22位之间，包含字符、数字和 _','^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]'),
+        ]
+        this.password2 = this.password1
+        this.nickname = [
+            new Rule('isLength','昵称不符合长度规范',{min:2,max:10}),
+        ]
+        
+    }
+
+    //我们可以通过用户传递过来的参数拿到 password1 和 password2 ，然后对二者的相等性进行校验,注意，该函数必须以 validate 开头
+    validatePassword(vals){
+        const psw1 = vals.body.password1
+        const psw2 = vals.body.password2
+        if(psw1 !== psw2){
+            throw new Error('两个密码必须相同')
+        }
+    }
+}
+
 module.exports = {
     PositiveIntegerValidator
 }
