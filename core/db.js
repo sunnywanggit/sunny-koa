3de1e6942ef2187d47f0d01487f340739de8366d
ergouwang <330035385@qq.com>
2020-10-12 22:50:16 +0800

    链接数据库相关配置

---
+++ core/db.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+const {dbName,host,port,user,password} = require('../config/config').database
+
+const sequelize = new Sequelize(dbName,user,password,{
+    //指定数据库类型
+    dialect:'mysql',
+    host,
+    port,
+    //是否在命令行显示具体的 sql 操作
+    logging:true,
+    //使用北京时间来记录相关时间方面的数据
+    timezone:'+08:00',
+    define:{
+    }
+})
+
+module.exports = {sequelize}
