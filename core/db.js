041fea15e8d085d506770d3b91404b4428e395d5
ergouwang <330035385@qq.com>
2020-10-17 16:17:16 +0800

    链接数据库

--- core/db.js
+++ core/db.js
@@ -11,7 +11,19 @@ const sequelize = new Sequelize(dbName,user,password,{
     //使用北京时间来记录相关时间方面的数据
     timezone:'+08:00',
     define:{
+        //carete_time updated_time delete_time
+        timestamps:true,
+        paranoid:true,
+        createdAt:'created_at',
+        updatedAt:'updated_at',
+        deletedAt:'deleted_at',
+        underscored:true
     }
+})
+
+//无需重新创建表即可添加新的字段
+sequelize.sync({
+    force:true
 })
 
 module.exports = {sequelize}
