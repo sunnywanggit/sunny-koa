7aea94d409cb9f94250d8ee00d11cf0cf2775e6f
ergouwang <330035385@qq.com>
2020-10-17 16:15:11 +0800

    建立 user 用户模型

--- app/modules/user.js
+++ app/modules/user.js
@@ -1,5 +1,26 @@
 const {Sequelize,Model} = require('sequelize')
-const {sequelize : db} = require('../../core/db')
+const {sequelize} = require('../../core/db')
+
+class User extends Model{ }
+
+User.init({
+    id:{
+        type:Sequelize.INTEGER,
+        //主键最基本的两个条件：不能重复、不能为空
+        primaryKey:true,
+        autoIncrement:true
+    },
+    nickname:Sequelize.STRING,
+    email:Sequelize.STRING,
+    password:Sequelize.STRING,
+    openid:{
+        type:Sequelize.STRING(64),
+        unique:true,
+    }
+},{
+    sequelize,
+    tableName:'user'
+})
 
 
 
