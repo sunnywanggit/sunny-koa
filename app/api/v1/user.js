c8eb9c5d62a4263c9fd69fb941abd6bf54ff5dc0
ergouwang <330035385@qq.com>
2020-10-17 20:31:12 +0800

    将用户注册信息写入数据库

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,4 +1,6 @@
 const Router = require('koa-router')
+const {User} = require('../../modules/user')
+
 
 //路有前缀，省略每一次都要写路由前缀
 const router = new Router({
@@ -10,7 +12,12 @@ const {RegisterValidator} = require('../../validators/validator')
 //新增注册功能
 router.post('/register',async (ctx)=>{
     const v = await new RegisterValidator().validate(ctx)
-
+    const user = {
+        nickname:v.get('body.nickname'),
+        email:v.get('body.email'),
+        password:v.get('body.password2'),
+    }
+    const result = await User.create(user)
 })
 
 module.exports = router
