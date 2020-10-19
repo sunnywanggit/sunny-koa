de3d2bac4438fcfe756be98178d8c999953ba9fd
ergouwang <330035385@qq.com>
2020-10-19 08:22:19 +0800

    使用观察者模式优化加密逻辑

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -11,16 +11,11 @@ const {RegisterValidator} = require('../../validators/validator')
 //新增注册功能
 router.post('/register',async (ctx)=>{
     const v = await new RegisterValidator().validate(ctx)
-    //这个10的意思是说计算机在生成盐的时候所花费的成本，你可以理解成这个数字越大，计算机生成盐所花费的时间和成本就越来越大
-    const salt = bcrypt.genSaltSync(10)
-    //对原有密码进行加密，生成新的密码
-    const password = bcrypt.hashSync(v.get('body.password2'),salt)
-    
     const user = {
         nickname:v.get('body.nickname'),
         email:v.get('body.email'),
-        //我们这里直接使用加密之后的密码
-        password:password,
+        //当我们对 password 赋值的时候，sequelize 自动就会调用 User 模型里面 passwrod 字段的 set 方法
+        password:v.get('body.password2'),
     }
     const result = await User.create(user)
 })
--- app/modules/user.js
+++ app/modules/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const {Sequelize,Model} = require('sequelize')
 const {sequelize} = require('../../core/db')
 
@@ -15,7 +16,19 @@ User.init({
         type:Sequelize.STRING(128),
         unique:true,
     },
-    password:Sequelize.STRING,
+    //set 方法就是典型的观察者模式的应用
+    password:{
+        type:Sequelize.STRING,
+        //只要我们对 password 进行赋值就会调用 set 方法
+        set(val){
+            //这个10的意思是说计算机在生成盐的时候所花费的成本，你可以理解成这个数字越大，计算机生成盐所花费的时间和成本就越来越大
+            const salt = bcrypt.genSaltSync(10)
+            //对原有密码进行加密，生成新的密码
+            const password = bcrypt.hashSync(val,salt)
+            //这样子就可以把加密过之后的密码保存到数据中库去了，同时需要说明的是 setDataValue 是 Model 上的方法,所以我们能直接使用 this 调用
+            this.setDataValue('passwrod',password)
+        }
+    } ,
     openid:{
         type:Sequelize.STRING(64),
         unique:true,
