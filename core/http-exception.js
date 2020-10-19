5d24dda2f3caea4193bba1c9d0fe7ebdc0df5c1e
ergouwang <330035385@qq.com>
2020-10-19 11:56:00 +0800

    Success 操作成功处理

--- core/http-exception.js
+++ core/http-exception.js
@@ -1,3 +1,4 @@
+
 //HttpException 之所以要继承 Error ,是因为我们要抛出 error ，只有是 Error 类型的才能抛出 error
 class HttpException extends Error{
     constructor(msg='服务器异常',errorCode=10000,code=400){
@@ -18,5 +19,27 @@ class ParameterException extends HttpException{
     }
 }
 
+class Success extends HttpException{
+    constructor(msg,errorCode){
+        super()
+        //201 操作成功 200 是查询成功的一个标识 ,如果说你提交了一个数据那么应该使用 201 这个状态码
+        this.code = 201
+        this.msg = msg || '参数错误'
+        this.errorCode = errorCode || 10000
 
-module.exports = {HttpException,ParameterException}
+    }
+}
+
+module.exports = {
+    HttpException,
+    ParameterException,
+    Success
+}
+
+
+
+
+
+
+
+
